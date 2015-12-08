509cd39825dd23462ec698bb8c3ea9fe24c594d7
jonathanhinton <hintonjonathan@gmail.com>
2015-12-07 19:45:47 -0600

    recording and controls working...minimally

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,21 +1,43 @@
-console.log("Hello");
-var mediaConstraints = {
-  audio: true
-};
-
-navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
-
-function onMediaSuccess(stream) {
-  var mediaRecorder = new MediaStreamRecorder(stream);
-  mediaRecorder.mimeType = 'audio/ogg';
-  mediaRecorder.audioChannels = 1;
-  mediaRecorder.ondataavailable = function (blob) {
-    var blobURL = URL.createObjectURL(blob);
-    document.write('<a href="' + blobURL + '">' + blobURL + '</a>');
+$(document).ready(function(){
+  console.log("Hello");
+  var mediaConstraints = {
+    audio: true
   };
-  mediaRecorder.start(3000);
-}
 
-function onMediaError(e) {
-  console.log("Media Error", e);
-}
+  navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
+
+  function onMediaSuccess(stream) {
+    var mediaRecorder = new MediaStreamRecorder(stream);
+    mediaRecorder.mimeType = 'audio/ogg';
+    mediaRecorder.audioChannels = 2;
+    mediaRecorder.ondataavailable = function (blob) {
+      var blobURL = URL.createObjectURL(blob);
+      $("#output").append('<a href="' + blobURL + '">' + blobURL + '</a>');
+    };
+    mediaRecorder.start(5000);
+
+    $("#stopRecording").on('click', function(){
+      mediaRecorder.stop();
+      console.log("Recording Stopped");
+    });
+
+    $("#pauseRecording").on('click', function(){
+      mediaRecorder.pause();
+    });
+
+    $("#resumeRecording").on('click', function(){
+      mediaRecorder.resume();
+    });
+
+    $("#saveRecording").on('click', function(){
+      mediaRecorder.save();
+    });
+
+  }
+
+
+  function onMediaError(e) {
+    console.log("Media Error", e);
+  }
+
+});
