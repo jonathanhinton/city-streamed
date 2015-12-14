4ff52766eaed066df05387b1a0eaa95831e45565
jonathanhinton <hintonjonathan@gmail.com>
2015-12-14 17:01:37 -0600

    timer working on mousedown

--- javascripts/app.js
+++ javascripts/app.js
@@ -56,9 +56,40 @@ $(document).ready(function(){
         $("#output").append('<audio preload="auto" src="' + blobURL + '" controls=""></audio>');
       };
 
+    //MOUSEDOWN begins transmission
       $("#transmit").mousedown(function(){
         mediaRecorder.start(15000);
+        //temporary countdown provided by http://jsfiddle.net/mrwilk/qVuHW/
+        function countdown( elementName, minutes, seconds )
+          {
+              var element, endTime, hours, mins, msLeft, time;
 
+              function twoDigits( n )
+              {
+                  return (n <= 9 ? "0" + n : n);
+              }
+
+              function updateTimer()
+              {
+                  msLeft = endTime - (+new Date());
+                  if ( msLeft < 1000 ) {
+                      element.innerHTML = "countdown's over!";
+                  } else {
+                      time = new Date( msLeft );
+                      hours = time.getUTCHours();
+                      mins = time.getUTCMinutes();
+                      element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+                      setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+                  }
+              }
+
+              element = document.getElementById( elementName );
+              endTime = (+new Date()) + 1000 * (60*minutes + seconds) + 500;
+              updateTimer();
+          }
+
+          countdown( "countdown", 0, 15 );
+      //MOUSEUP ends transmission
         $("#transmit").mouseup(function(){
           mediaRecorder.stop();
         });
